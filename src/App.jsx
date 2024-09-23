82b7a5c1d313760deb8973ac0b0fa544cbd5f9a5
Cristina Aguilera <developer.c.93.c@gmail.com>
2024-09-23 21:20:23 +0200

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import ExponentSix from "./components/ExponentSix";
 
 
 function App () {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
